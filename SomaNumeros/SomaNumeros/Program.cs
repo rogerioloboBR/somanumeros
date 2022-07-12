0757181cbe852328f242754bab9dfc954d30a850
Rogério Lobo <matusael5004@hotmail.com>
2022-07-12 16:35:29 -0300

    Add files via upload
    first cmmit

---
+++ SomaNumeros/SomaNumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Por favor digite um número:");
+int num = Int32.Parse(Console.ReadLine());
+int num1 = 3;
+int num2 = 5;
+int somaNum1 = 0;
+int somaNum2 = 0;
+int res;
+
+
+for (int i = 0; i < num; i++)
+{
+    if(i % num1 == 0)
+    {
+        somaNum1 += i;
+    }
+}
+
+for (int i = 0; i < num; i++)
+{
+    if (i % num2 == 0)
+    {
+        somaNum2 += i;
+    }
+}
+res = somaNum1 + somaNum2;
+
+Console.WriteLine("A soma dos multiplos de 3 e 5 até "+num+ ",é: " + res);
+Console.ReadKey();  
